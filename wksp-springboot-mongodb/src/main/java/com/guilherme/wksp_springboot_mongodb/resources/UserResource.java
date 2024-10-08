4523552fe51d2271e6a77f0028f94e966aaeaa0a
Guilherme Klabunde <guiandrei17@gmail.com>
2024-10-08 14:24:09 -0300

    Conectando ao MongoDB com repository e service

--- wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/resources/UserResource.java
+++ wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/resources/UserResource.java
@@ -1,28 +1,26 @@
 package com.guilherme.wksp_springboot_mongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.wksp_springboot_mongodb.domain.User;
+import com.guilherme.wksp_springboot_mongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
     
+	@Autowired
+	private UserService service;
+	
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        User maria = new User(null, "Maria Silva", "maria@gmail.com");
-        User guilherme = new User(null, "Guilherme Klabunde", "guiandrei17@gmail.com");
-        
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, guilherme));
-        
+    public ResponseEntity<List<User>> findAll() {      
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     	}
 }
