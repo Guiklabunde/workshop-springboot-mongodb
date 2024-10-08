46ff737d524cb685e6e28b597ef413dee55295a1
Guilherme Klabunde <guiandrei17@gmail.com>
2024-10-08 11:58:48 -0300

    First commit

---
+++ wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/WkspSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.wksp_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkspSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkspSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ wksp-springboot-mongodb/src/test/java/com/guilherme/wksp_springboot_mongodb/WkspSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.wksp_springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkspSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
