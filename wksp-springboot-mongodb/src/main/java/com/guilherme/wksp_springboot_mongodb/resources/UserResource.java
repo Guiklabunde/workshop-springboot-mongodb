4e0d1786a09eccd4798cee2349403104aa672cf7
Guilherme Klabunde <guiandrei17@gmail.com>
2024-10-08 15:19:53 -0300

    Obtendo um usuário por id

--- wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/resources/UserResource.java
+++ wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +18,19 @@ import com.guilherme.wksp_springboot_mongodb.services.UserService;
 @RequestMapping(value = "/users")
 public class UserResource {
     
-	@Autowired
-	private UserService service;
-	
+    @Autowired
+    private UserService service;
+    
     @GetMapping
-    public ResponseEntity<List<UserDTO>> findAll() {      
+    public ResponseEntity<List<UserDTO>> findAll() {  // Corrigido o tipo de retorno
         List<User> list = service.findAll();
-        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList()); // Usando método de referência
         return ResponseEntity.ok().body(listDto);
+    }
+    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/services/UserService.java
+++ wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.guilherme.wksp_springboot_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.wksp_springboot_mongodb.domain.User;
 import com.guilherme.wksp_springboot_mongodb.repository.UserRepository;
+import com.guilherme.wksp_springboot_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+	    Optional<User> optionalUser = repo.findById(id);
+	    return optionalUser.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
 }
