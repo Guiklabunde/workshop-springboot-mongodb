5409ea74730329c074df8f364905d6cfe6e8f481
Guilherme Klabunde <guiandrei17@gmail.com>
2024-10-09 16:35:31 -0300

    Obtendo POST por ID

---
+++ wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.guilherme.wksp_springboot_mongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.wksp_springboot_mongodb.domain.Post;
+import com.guilherme.wksp_springboot_mongodb.repository.PostRepository;
+import com.guilherme.wksp_springboot_mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository repo;
+    
+    public List<Post> findAll() {
+        return repo.findAll();
+    }
+    
+    public Post findById(String id) {
+        return repo.findById(id).orElseThrow(() -> 
+            new ObjectNotFoundException("Objeto não encontrado")
+        );
+    }
+}
