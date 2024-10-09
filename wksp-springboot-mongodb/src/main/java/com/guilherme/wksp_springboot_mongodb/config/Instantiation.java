59aaf3289f64db5173985a646cf1beafd571a757
Guilherme Klabunde <guiandrei17@gmail.com>
2024-10-09 14:27:06 -0300

    Projeção dos dados do autor com DTO

--- wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/config/Instantiation.java
+++ wksp-springboot-mongodb/src/main/java/com/guilherme/wksp_springboot_mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.guilherme.wksp_springboot_mongodb.domain.Post;
 import com.guilherme.wksp_springboot_mongodb.domain.User;
+import com.guilherme.wksp_springboot_mongodb.dto.AuthorDTO;
 import com.guilherme.wksp_springboot_mongodb.repository.PostRepository;
 import com.guilherme.wksp_springboot_mongodb.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
         User guilherme = new User(null, "Guilherme Andrei", "guiandrei17@gmail.com");
         User senna = new User(null, "Ayrton", "dobrasil@gmail.com");
         
-        Post post1 = new Post(null, sdf.parse("21/03/2025"), "Partiu Las Vegas", "Vou para os EUA, abralho", guilherme);
-        Post post2 = new Post(null, sdf.parse("17/11/2025"), "Partiu Paris", "Visitar a França com a muié, comemorar o niver.", guilherme);
-        
         userRepository.saveAll(Arrays.asList(maria, guilherme, senna));
+        
+        Post post1 = new Post(null, sdf.parse("21/03/2025"), "Partiu Las Vegas", "Vou para os EUA, abralho", new AuthorDTO(guilherme));
+        Post post2 = new Post(null, sdf.parse("17/11/2025"), "Partiu Paris", "Visitar a França com a muié, comemorar o niver.", new AuthorDTO(guilherme));
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
